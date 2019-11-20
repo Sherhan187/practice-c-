825f7825f08b32ace75af6f7925b19b950729b2b
Sherhan187 <57889104+Sherhan187@users.noreply.github.com>
2019-11-20 22:22:02 +0300

    Create 53.cpp

---
+++ 53.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+int main () {
+    int a,b;
+    cin>>a>>b;
+    if (a>30 || b>30){
+        cout<<"Yes bro"<<endl;
+    }
+else {
+    cout<<"no bro"<<endl;
+}
+    return 0;
+}
