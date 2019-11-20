5493abf7fc86f61b63c52beb2cf01be3477a3181
Sherhan187 <57889104+Sherhan187@users.noreply.github.com>
2019-11-20 22:01:41 +0300

    Create 52main.cpp

---
+++ 52main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+int main () {
+    int a,b,c,d; 
+    cin>>a>>b>>c>>d;
+    if (a>5 && b>5 && c/6 && d%3){
+     cout<<"Yes bro"<<endl;
+     }
+    else {
+        cout<<"no bro"<<endl;
+    }
+    return 0;
+}
