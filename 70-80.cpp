f74c1f7cc154346c97490820286dfc0ab747fbea
Sherhan187 <57889104+Sherhan187@users.noreply.github.com>
2019-11-21 11:56:14 +0600

    Create 70-80.cpp

---
+++ 70-80.cpp
@@ -0,0 +1,54 @@
+71**
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int h, m;
+    cin>>h>>m;
+    cout<<0.5*abs(60*h-11*m)<<"degree";
+    return 0;
+}
+75**
+#include <iostream>
+using namespace std;
+int main(){
+    string text="You are welcome";
+    for(int i=0; i<10;i++){
+        cout<<text<<endl;
+    }
+}
+
+76**
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string text="Silence is golden";
+    for(int i=0; i<n;i++){
+        cout<<text<<endl;
+    }
+}
+
+79**
+#include <iostream>
+using namespace std;
+int main(){
+    int n=0;
+    for(int i=0; i<20;i++){
+        n++;
+    cout<<n<<" ";
+    }
+}
+
+80**
+#include <iostream>
+using namespace std;
+int main (){
+   int number;
+    cin>>number;
+    for(int i=0; i<8; i++){
+        number+=3;
+        cout<<number<<" ";
+    }
+    return 0;
+}
